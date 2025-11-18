d7028b1f28ca629858ef11f3668e27442c1430b3
PARKSEONAH251 <161944840+PARKSEONAH251@users.noreply.github.com>
2025-11-18 13:56:58 +0900

    로그인 페이지, 회원가입 페이지 경로설정완료

--- gptmedicine-web/src/App.js
+++ gptmedicine-web/src/App.js
@@ -2,13 +2,16 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginJoin from "./page/LoginJoin";
 import Flashback from "./page/Flashback"; // 넘어가기 버튼이 있는 페이지 (예시)
-
+import Login from "./page/Login"; 
+import Signup from "./page/Signup";
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Flashback />} />         {/* 기본 페이지 */}
         <Route path="/loginjoin" element={<LoginJoin />} /> {/* 이동할 페이지 */}
+        <Route path="/login" element={<Login />} />         {/* 로그인 페이지 */}
+        <Route path="/signup" element={<Signup />} />       {/* 회원가입 페이지 */}
       </Routes>
     </Router>
   );

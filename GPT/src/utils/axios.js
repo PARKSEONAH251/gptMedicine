904ff1b6f22e47de838447d637dafca81177d921
ForPublicProject92 <riethe92@naver.com>
2025-11-30 15:35:10 +0900

    Nginx을 사용하도록 변경했습니다.
    서버주소는 "localhos:404" (Nginx)으로 변경했습니다.

--- GPT/src/utils/axios.js
+++ GPT/src/utils/axios.js
@@ -2,16 +2,13 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:2080", // 서버 base URL
+    baseURL: "http://localhost:404/",
 });
 
 // 요청 인터셉터: Authorization 자동 추가
 instance.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
-
-    if (token) {
-        config.headers["Authorization"] = `Bearer ${token}`;
-    }
+    if (token) config.headers["Authorization"] = `Bearer ${token}`;
     return config;
 });
 

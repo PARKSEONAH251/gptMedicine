297504c5fcd5beb360c6d2bb79bbabc6c093d95d
PARKSEONAH251 <161944840+PARKSEONAH251@users.noreply.github.com>
2025-11-30 21:18:03 +0900

    플래시백 스크롤 삭제

--- gptmedicine-web/src/page/Flashback.js
+++ gptmedicine-web/src/page/Flashback.js
@@ -12,21 +12,21 @@ export default function Flashback() {
 
   const scrollRef = useRef(null);
 
-  // 왼쪽 스크롤
-  const scrollLeft = () => {
-    scrollRef.current.scrollBy({
-      left: -300,
-      behavior: "smooth",
-    });
-  };
+  // // 왼쪽 스크롤
+  // const scrollLeft = () => {
+  //   scrollRef.current.scrollBy({
+  //     left: -300,ㅊ
+  //     behavior: "smooth",
+  //   });
+  // };
 
-  // 오른쪽 스크롤
-  const scrollRight = () => {
-    scrollRef.current.scrollBy({
-      left: 300,
-      behavior: "smooth",
-    });
-  };
+  // // 오른쪽 스크롤
+  // const scrollRight = () => {
+  //   scrollRef.current.scrollBy({
+  //     left: 300,
+  //     behavior: "smooth",
+  //   });
+  // };
 
   return (
     <div className="FlashBackContainer">
@@ -36,18 +36,18 @@ export default function Flashback() {
         <p className="FlashBack-welcome-text">환영합니다!</p>
         <p className="FlashBack-description-text">의료 AI 서비스 약찾고와 함께하세요.</p>
               <div className="carousel-container">
-        <button className="scroll-btn left" onClick={scrollLeft}>
+        {/* <button className="scroll-btn left" onClick={scrollLeft}>
           <ChevronLeft size={24} />
-        </button>
+        </button> */}
         <div className="carousel-content" ref={scrollRef}>
           <div className="item">
             <img src="/image/medicalsafe_logo.png" className="logo-image" alt="MedicalSafe Logo" />
           </div>
           <div className="item"></div>
         </div>
-        <button className="scroll-btn right" onClick={scrollRight}>
+        {/* <button className="scroll-btn right" onClick={scrollRight}>
           <ChevronRight size={24} />
-        </button>
+        </button> */}
       </div>
       </div>
       <button className="skip-button" onClick={handleSkip}>넘어가기</button>
